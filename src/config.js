718a24fb78d24f60688f766f50b3bf62b05e4eeb
nickjang <nickjang@alumni.upenn.edu>
2021-01-27 09:14:08 -0500

    Update cors

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: 'https://recipe-sharing-app.vercel.app' || 'http://localhost:3000',
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL
